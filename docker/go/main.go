c9b0a7660c3aa2655621e605146e8e192603a7b1
Gladstone <thiagogladstone@Gladstones-MacBook-Pro.local>
2022-12-28 15:53:51 -0300

    added logic create image go

--- docker/go/main.go
+++ docker/go/main.go
@@ -1,3 +1,17 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// )
+// func main() {
+//     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//         fmt.Fprintf(w, "Full Cycle Rocks!!")
+//     })
+//     log.Fatal(http.ListenAndServe(":8080", nil))
+// }
+
 package main
 
 import "fmt"
